adc05ae2abaf226be614bef126ae7ac815a9807a
Gabriel Chaves <gabovvv@gmail.com>
2022-08-02 21:28:05 -0600

    work section and popup window

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ let mixerPortfolio = mixitup('.work__container', {
     }
 });
 
-/*Link Active Work*/
+/*========Link Active Work============*/
 const linkWork = document.querySelectorAll('.work__item')
 
 function activeWork(){
@@ -37,3 +37,26 @@ function activeWork(){
 }
 
 linkWork.forEach(l => l.addEventListener("click", activeWork))
+
+
+/*========Work Popup============*/
+document.addEventListener("click", (e) => {
+
+    if(e.target.classList.contains("work__button")){
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
