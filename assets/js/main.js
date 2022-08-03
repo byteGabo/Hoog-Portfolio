70ef9228243f80073ef539dbf726ef63567e513c
Gabriel Chaves <gabovvv@gmail.com>
2022-08-02 20:34:42 -0600

    work section and portfolio filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,23 @@ const tabs = document.querySelectorAll('[data-target]'),
             tab.classList.add('skills__active')
         })
       })
+
+/*MIXITUP FILTER PORTFOLIO*/
+let mixerPortfolio = mixitup('.work__container', {
+    selectors:{
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*Link Active Work*/
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener("click", activeWork))
