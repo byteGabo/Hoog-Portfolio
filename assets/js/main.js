4b7509cd4b44714b0d161ed2bdfebc0a1ffe5122
Gabriel Chaves <gabovvv@gmail.com>
2022-08-03 21:14:29 -0600

    scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,28 @@ let swiper = new Swiper(".testimonials__container", {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);
   })
+
+  /*===============SCROLL SECTIONS ACTIVE LINK=============*/
+  //get all sections that have an id defined
+  const sections = document.querySelectorAll("section[id]");
+
+  //add an event listener listening for scroll
+  window.addEventListener("scroll", navHighlighter);
+
+  function navHighlighter(){
+    //get current scroll position
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop -50;
+        sectionId = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
+        {
+           document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link") 
+        }
+        else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+    })
+  }
